850b3c054ef2c5916f464a2fbe8fa6c6a3cf84f6
ciarellipm <simply_pmc@yahoo.com.br>
2024-04-20 06:01:02 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
+import { useEffect } from 'react';
+import Graph from "graphology";
+import { cropToLargestConnectedComponent } from "graphology-components";
+import forceAtlas2 from "graphology-layout-forceatlas2";
+import circular from "graphology-layout/circular";
+import Papa from "papaparse";
+import Sigma from "sigma";
+import chroma from 'chroma-js';
 
 
 export const App = () => {
+  let renderer: Sigma | null = null;
+
+  const graph = new Graph();
+  graph.addNode("1", { label: "Node 1", x: 0, y: 0, size: 10, color: "blue" });
+  graph.addNode("2", { label: "Node 2", x: 1, y: 1, size: 20, color: "red" });
+  graph.addEdge("1", "2", { size: 5, color: "purple" });
+
+  //const renderer = new Sigma(
+  //  graph,
+  //  document.getElementById("sigma") as HTMLDivElement
+  //);
+
+  // Hide the loader from the DOM
+  const loader = document.getElementById("root") as HTMLElement;
+  if (loader) loader.style.display = "none";
+
+  // Draw the final graph using sigma 
+  const container = document.getElementById("sigma") as HTMLElement;
+  if (renderer === null) renderer = new Sigma(graph, container, { allowInvalidContainer: true });
+
   return (
-    <div className="App">
-      Hello
+    <div>
+      <div id="loader">Loading...</div>
     </div>
   );
 }
+
 
 
 
